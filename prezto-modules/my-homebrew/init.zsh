738409c12fa65b98557135d6304d6e2fd3f1b9d3
Kevin Wurster <wursterk@gmail.com>
2024-10-22 08:40:17 -0400

    Getting CPU count is hard - just manually set when needed

--- prezto-modules/my-homebrew/init.zsh
+++ prezto-modules/my-homebrew/init.zsh
@@ -3,7 +3,3 @@
 # Homebrew sends anonymous metrics about usage by default.
 export HOMEBREW_NO_ANALYTICS=1
 
-# For some reason this does not work:
-#   export HOMEBREW_MAKE_JOBS=$(echo "$NCPU" | awk "{printf int(int($1) * 0.75)}")
-# but this does:
-export HOMEBREW_MAKE_JOBS=$(printf %.0f $(echo "$(sysctl -n hw.ncpu) * 0.75" | bc))
